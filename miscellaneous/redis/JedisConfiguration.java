d85c4c2fd75c5b6abd40601967b2aa6799d1c1c9
HONEY KHATRI <honeykhatri5@gmail.com>
2021-07-29 00:41:41 +0530

    Create JedisConfiguration.java

---
+++ miscellaneous/redis/JedisConfiguration.java
@@ -0,0 +1,51 @@
+package com.gmt.userdocumenttranslation.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+@Configuration
+public class JedisConfiguration {
+    @Value("spring.redis.host")
+    public String redisHostname;
+
+    @Value("spring.redis.password")
+    public String redisPassword;
+
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("gmt-redis-dev.redis.cache.windows.net",
+                6379);
+        redisStandaloneConfiguration.setPassword("UVN7QmfqCRIR2Zo6tmtmMZz9rmRmpFGS+W1dTnYoeBg=");
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+
+    }
+
+//    @Bean
+//    RedisConnectionFactory jedisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("gmt-redis-dev.redis.cache.windows.net",
+//                6380);
+////        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().useSsl().build();
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of("UVN7QmfqCRIR2Zo6tmtmMZz9rmRmpFGS+W1dTnYoeBg="));
+//        return new JedisConnectionFactory(redisStandaloneConfiguration);
+//
+//
+//
+//    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+
+}
